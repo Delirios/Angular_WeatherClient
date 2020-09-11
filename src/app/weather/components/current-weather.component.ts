9a5f65a0559f4c8003078d1815ab2d332f74776b
Delirios <lushchevskiyd@gmail.com>
2020-09-11 21:38:58 +0300

    adding weather search for the selected city

--- src/app/weather/components/current-weather.component.ts
+++ src/app/weather/components/current-weather.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit} from '@angular/core';
 import { IRoot } from './../models/current-weather';
 import { _MatTabNavBase } from '@angular/material/tabs';
 
+
 @Component({
     selector: 'wr-weather',
     templateUrl: './current-weather.component.html',
@@ -17,29 +18,30 @@ export class CurrentWeatherComponent implements OnInit{
             this.lon = pos.lon;
             this.lat = pos.lat;
             this.weatherService.getWeatherByLocation(this.lat, this.lon)
-            .subscribe((data : IRoot) => this.current_weather = 
+            .subscribe((data : IRoot) => this.current_location_weather = 
             {
                 name: (data as any).name,
                 main : (data as any ).main,
                 wind : (data as any).wind,
                 sys: (data as any).sys,
                 weather: (data  as any).weather
+                
             })
+
             });
     }
    
     constructor(private weatherService : WeatherService ){}
+    current_weather: IRoot;
+    current_location_weather : IRoot
+    
     lon : number;
     lat : number;
-
+    desc : string;
     cityName:string;
     weather : any [];
-
     errorMessage = '';
-    current_weather: IRoot;
-    Show(){
-        console.log(this.current_weather.main.humidity)
-}
+
     ShowWeather(cityName){
         this.weatherService.getWeather(this.cityName)
         .subscribe((data : IRoot) => this.current_weather = {
